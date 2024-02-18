73fbf9e40079582f954ee738265b56e7701227c3
adfea625 <adfea625@hotmail.com>
2024-02-18 22:36:28 +0100

    error: Objects have the same fully qualified name but different paths. New Object: GDALDataset

--- Source/GDAL/GDAL.Build.cs
+++ Source/GDAL/GDAL.Build.cs
@@ -1,157 +1,70 @@
-
 using System.IO;
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class GDAL : ModuleRules
 {
-	public GDAL(ReadOnlyTargetRules Target) : base(Target)
-	{
-		Type = ModuleType.External;
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		//Add include directory
-		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+    public GDAL(ReadOnlyTargetRules Target) : base(Target)
+    {
+        Type = ModuleType.External;
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		if (Target.Platform == UnrealTargetPlatform.Win64) {
-			//add shared libs.
-			var libs = Directory.GetFiles(Path.Combine(ModuleDirectory, "lib"), "*" + ".lib", SearchOption.AllDirectories);
-			foreach (string lib in libs)
-			{
-				PublicAdditionalLibraries.Add(lib);
-			}
+        // Common include directory for all platforms
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
 
-			//add dlls
-			var dlls = new List<string>(Directory.GetFiles(Path.Combine(ModuleDirectory, "lib"), "*" + ".dll"));
-			dlls.AddRange(Directory.GetFiles(Path.Combine(ModuleDirectory, "lib", "gdalplugins"), "*" + ".dll"));
-			dlls.AddRange(Directory.GetFiles(Path.Combine(ModuleDirectory, "bin"), "*" + ".dll"));
-			string stagingDir = Path.Combine("$(ProjectDir)", "Binaries", "Data", "GDAL");
-			string binaryStagingDir = Path.Combine("$(ProjectDir)", "Binaries", "Win64");
+        if (Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            // Adding the lib directory to the library paths
+            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "lib"));
 
-			foreach (string dll in dlls)
-			{
-				//Console.WriteLine(dll);
-				RuntimeDependencies.Add(Path.Combine(binaryStagingDir, Path.GetFileName(dll)), dll, StagedFileType.NonUFS);
-			}
+            // Adding the dynamic GDAL library
+            string DynamicGDALLib = Path.Combine(ModuleDirectory, "lib", "libgdal.so");
+            PublicAdditionalLibraries.Add(DynamicGDALLib);
 
-			var datadirs = Directory.GetDirectories(Path.Combine(ModuleDirectory, "share"), "*");
+            // Staging directory for GDAL data files
+            string stagingDir = Path.Combine("$(ProjectDir)", "Binaries", "Data", "GDAL");
+            var datadirs = Directory.GetDirectories(Path.Combine(ModuleDirectory, "share"), "*");
 
-			if (!Directory.Exists(stagingDir))
-			{
-				Directory.CreateDirectory(stagingDir);
-			}
-			foreach (string dir in datadirs)
-			{
-				string StagedDataDir = Path.Combine(stagingDir, Path.GetFileName(dir));
-				System.Console.WriteLine(StagedDataDir);
+            if (!Directory.Exists(stagingDir))
+            {
+                Directory.CreateDirectory(stagingDir);
+            }
+            foreach (string dir in datadirs)
+            {
+                string StagedDataDir = Path.Combine(stagingDir, Path.GetFileName(dir));
+                if (!Directory.Exists(StagedDataDir))
+                {
+                    Directory.CreateDirectory(StagedDataDir);
+                }
+                foreach (string file in Directory.GetFiles(dir, "*"))
+                {
+                    RuntimeDependencies.Add(Path.Combine(StagedDataDir, Path.GetFileName(file)), file, StagedFileType.NonUFS);
+                }
+            }
 
-				if (!Directory.Exists(StagedDataDir))
-				{
-					Directory.CreateDirectory(StagedDataDir);
-				}
-				foreach (string file in Directory.GetFiles(dir, "*"))
-				{
-					RuntimeDependencies.Add(Path.Combine(StagedDataDir, Path.GetFileName(file)), file, StagedFileType.NonUFS);
+            // Dependencies include path
+            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "dependencies", "include"));
 
-				}
+            // Adding modules GDAL may depend on, assuming your project correctly sets these up as dependencies.
+            PublicDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "Core",
+                }
+            );
 
-
-			}
-
-
-			//stage dependencies
-			PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "dependencies", "include"));
-
-			var dependencydlls = Directory.GetFiles(Path.Combine(ModuleDirectory, "dependencies"), "*.dll");
-			var dependencylibs = Directory.GetFiles(Path.Combine(ModuleDirectory, "dependencies"), "*.lib");
-
-			foreach (var  dll in dependencydlls)
-			{
-				RuntimeDependencies.Add(Path.Combine(binaryStagingDir, Path.GetFileName(dll)), dll, StagedFileType.NonUFS);
-			}
-			foreach (var  lib in dependencylibs)
-			{
-				PublicAdditionalLibraries.Add(lib);
-			}
-
-
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"Core",
-				}
-			);
-			
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"CoreUObject",
-					"Engine",
-					"libcurl", //link against these modules so GDAL can use them.
-					"SQLiteCore",
-					"LibTiff",
-					"ZLib",
-				}
-			);
-
-		}
-		else if (Target.Platform == UnrealTargetPlatform.Linux)
-		{
-			// This is for a dynamic/shared build of GDAL. (Whereas the Win64 branch above is for a static build of GDAL.)
-
-			string DynamicGDALLib = Path.Combine(ModuleDirectory, "lib", "libgdal.so");
-			PublicAdditionalLibraries.Add(DynamicGDALLib);
-			
-			string stagingDir = Path.Combine("$(ProjectDir)", "Binaries", "Data", "GDAL");
-			var datadirs = Directory.GetDirectories(Path.Combine(ModuleDirectory, "share"), "*"); 
-
-			if (!Directory.Exists(stagingDir))
-			{
-				Directory.CreateDirectory(stagingDir);
-			}
-			foreach (string dir in datadirs)
-			{
-				string StagedDataDir = Path.Combine(stagingDir, Path.GetFileName(dir));
-				// System.Console.WriteLine("StagedDataDir: " + StagedDataDir);
-				if (!Directory.Exists(StagedDataDir))
-				{
-					Directory.CreateDirectory(StagedDataDir);
-				}
-				foreach (string file in Directory.GetFiles(dir, "*"))
-				{
-					RuntimeDependencies.Add(Path.Combine(StagedDataDir, Path.GetFileName(file)), file, StagedFileType.NonUFS);
-				}
-			}
-
-			PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "dependencies", "include"));
-
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"Core",
-				}
-			);
-			
-			// Go to UE5 root directory and use 'find . -iname "*.build.cs" | grep <blabla>' for these.
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"CoreUObject",
-					"Engine",
-					"PROJ", //link against these modules so GDAL can use them.
-					"libcurl",
-					"SQLiteCore",
-					"LibTiff",
-					"zlib",
-					"UElibPNG",
-					"LibJpegTurbo",
-					"Expat",
-					"libxml2",
-					"OpenSSL",
-					"nghttp2"
-				}
-			);
-		}
-
-	}
+            // These are common dependencies GDAL might use. You may need to adjust this list based on your actual dependencies and third-party libraries.
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "CoreUObject",
+                    "Engine",
+                    "Projects",
+                    "RHI",
+                    "RenderCore",
+                    // Add other dependencies required by GDAL here
+                }
+            );
+        }
+    }
 }
